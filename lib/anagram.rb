9fc3c314d82670436bfc899e7a6c304014802c7b
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-13 17:37:18 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_Accessor :word
+
+  def initialize(word)
+    anagram = []
+
+
+  def match(array)
+    anagram = []
+
+    array.each do |word|
+      if word.split("").sort == self.word.split("").sort
+        anagram << word
+      end 
+    end
+
+    anagram
+  end
+end
